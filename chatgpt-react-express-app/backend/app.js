468af971eec0db732d4572d23a7fd6f16b7b6cd3
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2023-01-12 22:18:22 -0500

    feature: consuming openai api

--- chatgpt-react-express-app/backend/app.js
+++ chatgpt-react-express-app/backend/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+// OpenAI Configuration
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const app = express();
 
@@ -9,12 +18,25 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.json({ status: "ok", message: "Welcome to the API!"});
 });
 
-app.post("/chatgpt", (req, res) => {
-  res.json({ status: "ok", message: "Hi from ChatGPT" })
+app.post("/chatgpt", async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message,
+    max_tokens: 1000,
+    temperature: 0,
+  });
+  console.log(response.data);
+  if (!response.data || !response.data.choices)
+    return res.json({ status: "error", message: "Internal Server Error"});
+  return res.json({
+    status: "ok",
+    message: response.data.choices[0].text
+  });
 });
 
 module.exports = app;
